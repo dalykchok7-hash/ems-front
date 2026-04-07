aab5470d628195cc19ca1ee0d743a4033a4e4f39
kchokdaly7@gmail.com <kchokmedali1@gmail.com>
2026-04-07 20:53:21 +0100

    Chore: Vercel deployment configuration

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://ems-w82z.onrender.com/api'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: false,
-   //apiUrl: 'https://ems-service-ndb1.onrender.com/api'
-   apiUrl: 'https://ems-w82z.onrender.com/api'
-
-  //apiUrl: 'http://localhost:8000/api'
+  apiUrl: 'http://localhost:8000/api'
   
 };
